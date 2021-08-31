0e514e527cbbee6708a404ebae82887aa5a77a08
sounakOffice <89705693+sounakOffice@users.noreply.github.com>
2021-08-31 16:16:21 +0530

    Cannot place original token here.
    Replace validGiHubAccessToken with the token given in documentation sample input to run the test.

--- GitHubApiAccess/GitHubApiTest/GitHubUnitTest.cs
+++ GitHubApiAccess/GitHubApiTest/GitHubUnitTest.cs
@@ -12,7 +12,7 @@ namespace GitHubApiTest
         {
             // Arrange
             GitHubApiAccess.Model.InputParameter inputValues = new GitHubApiAccess.Model.InputParameter { UserName = "sounakOffice"
-                , RepoName = "SampleRepo", UserToken = "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" };
+                , RepoName = "SampleRepo", UserToken = "validGiHubAccessToken" };
 
             bool expectedValue = true;
             bool actualValue = false;
